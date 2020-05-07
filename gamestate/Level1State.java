ba74d2f6caafe33270f9c0925d280a29b84b30b1
Alexander Eilert <57025593+Flamesorcer@users.noreply.github.com>
2020-05-06 22:37:45 -0500

    Update Level1State.java

--- gamestate/Level1State.java
+++ gamestate/Level1State.java
@@ -1,3 +1,4 @@
+
 package gamestate;
 
 import main.GamePanel;
@@ -9,7 +10,7 @@ import java.awt.event.KeyEvent;
 import objects.Block;
 import java.util.ArrayList;
 import mapping.Map;
-
+import resources.Images;
 public class Level1State extends GameState
 {
     
@@ -21,7 +22,7 @@ public class Level1State extends GameState
         
     }
     public void init(){
-        player = new Player(30, 150);
+        player = new Player(30, 150, this);
         xOffset = -200;
         yOffset = -200;
         map = new Map("/Maps/map1.map");
@@ -38,6 +39,26 @@ public class Level1State extends GameState
         g.fillRect(0,0, GamePanel.WIDTH, GamePanel.HEIGHT);
         player.draw(g);
         map.draw(g);
+        int unHealth = player.getHealth();
+        for (int i = 0; i < 10; i++){
+            if(unHealth <= 0){
+                g.drawImage(Images.getHearts()[2], 30 + 40 * i, 30, 30, 30, null);
+            }
+            else if(unHealth == 1){
+                g.drawImage(Images.getHearts()[1], 30 + 40 * i, 25, 30, 30, null);
+                unHealth -= 1;
+            }
+            else{
+                g.drawImage(Images.getHearts()[0], 30 + 40 * i, 20, 30, 30, null);
+                unHealth -= 2;
+            }
+        }
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Ariel", Font.PLAIN, 20));
+        g.drawString("Level 1", GamePanel.WIDTH - 75, 30);
+    }
+    public void nextLevel(){
+        gsm.states.push(new Level2State(gsm));
     }
     public  void keyPressed(int k){
         player.keyPressed(k);
